9dd12fcd0be5faaee79a1cd68602239046222393
ahmed gadi <ahmedgadi642@gmail.com>
2023-07-18 19:46:04 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,14 +3,18 @@
 *print_last_digit - Entry point
 * Description: 'last digit'
 * @n: Entered data
-* Return: always 0 (success)
+* Return: always ld (success)
 */
 int print_last_digit(int n)
 {
 	int ld;
 
 	ld = n % 10;
-	puts(ld);
-	puts(ld);
-	return (0);
+	if (ld < 0)
+	{
+		_putchar(-ld + 48);
+		return (-ld);
+	}
+	_putchar(ld + 48);
+	return (ld);
 }
