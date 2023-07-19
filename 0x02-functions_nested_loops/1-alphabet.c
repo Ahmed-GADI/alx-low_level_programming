3cf2331ca676a835a59b863f3fb13567638552d1
ahmed gadi <ahmedgadi642@gmail.com>
2023-07-18 18:21:43 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+* print_alphabet - Entry point
+* Description: 'prints alphabets'
+* Returm: always 0 (success)
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
