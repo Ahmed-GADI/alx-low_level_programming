e0f21b22900e262750ef33aa91e40c852ee92533
ahmed gadi <ahmedgadi642@gmail.com>
2023-07-18 19:34:47 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_last_digit - Entry point
+* Description: 'last digit'
+* @n: Entered data
+* Return: always 0 (success)
+*/
+int print_last_digit(int n)
+{
+	int ld;
+
+	ld = c % 10;
+	printf("%d", ld);
+	return (0);
+}
