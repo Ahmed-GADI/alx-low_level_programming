99344bc03671fb921b6c210efe92d8f5719acf0b
ahmed gadi <ahmedgadi642@gmail.com>
2023-07-18 19:19:05 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_sign - Entry point
+* Description: 'check if lower case'
+* @n: enterd data
+* Return: 1 (success)
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (0);
+	}
+	return (-1);
+}
