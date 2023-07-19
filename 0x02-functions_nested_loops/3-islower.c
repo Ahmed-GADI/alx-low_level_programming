eaa7ce916bbc8af4cc51fe6e8bdf1ce01be4e9bb
ahmed gadi <ahmedgadi642@gmail.com>
2023-07-18 19:03:23 -0700

    slower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_islower - Entry point
+* Description: 'check if lower case'
+* Return: 1 (success)
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
