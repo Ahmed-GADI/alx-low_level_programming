ea91000036e23218ee80c6806fa97e4c92694ead
ahmed gadi <ahmedgadi642@gmail.com>
2023-07-20 14:24:46 -0700

    more nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* more_numbers - Entry point
+*/
+
+void more_numbers(void)
+{
+	for (int i = 0 ; i <= 9 ; i++)
+	{
+		for (int j = 0 ; j <= 14 ; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
