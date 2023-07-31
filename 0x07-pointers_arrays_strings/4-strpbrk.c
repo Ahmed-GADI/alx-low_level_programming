f55d1fa9271807491134be347139608f85b2f6b6
ahmed gadi <ahmedgadi642@gmail.com>
2023-07-31 11:10:37 -0700

    more more pointers

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -15,11 +15,9 @@ char *_strpbrk(char *s, char *accept)
 	{
 		for (i = 0 ; accept[i] ; i++)
 		{
-			if ( *s == accept[i] )
-			{
+			if (*s == accept[i])
 				return (s);
-			}
-		}	
+		}
 		s++;
 	}
 	return ('\0');
