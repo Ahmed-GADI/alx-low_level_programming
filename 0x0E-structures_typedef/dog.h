83c1ab7bc4519119bd886d53da17698a6702c252
ahmed gadi <ahmedgadi642@gmail.com>
2023-08-14 18:20:11 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,18 @@
-struct dog{
-	char * name;
+#ifndef HEADER_FILE_NAME_H
+#define HEADER_FILE_NAME_H
+
+/**
+ * struct dog - short description
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
+
+#endif
