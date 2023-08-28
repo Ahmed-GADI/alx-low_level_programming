a07ab77cbc4ce5406555cf11670f3624f1c4010f
Ahmed GADI <ahmedgadi642@gmail.com>
2023-08-28 23:27:24 +0100

    more singly linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,11 +15,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (!new)
 		return (NULL);
-	else
-	{
-		new->n = n;
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
+	
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
