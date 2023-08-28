c3c53b065c31720e4d578d416c83a3a64b05d720
Ahmed GADI <ahmedgadi642@gmail.com>
2023-08-28 23:05:37 +0100

    more singly linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - Entry point
+ * @h: entered list
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t cmpt = 0;
+
+	while(h)
+	{
+		cmpt++;
+		h = h->next;
+	}
+	return (cmpt);
+}
