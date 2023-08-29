122b1d06421f3cad2f55735467c1d3d3c677b66c
Ahmed GADI <ahmedgadi642@gmail.com>
2023-08-29 18:03:38 +0100

    More singly linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * pop-listint - Entry point
+ * @head: pointer to the head of the list
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp = *head;
+	int ret;
+
+	ret = tmp->n;
+	(*head) = (*head)->next;
+	free(tmp);
+	return (ret);
+}
