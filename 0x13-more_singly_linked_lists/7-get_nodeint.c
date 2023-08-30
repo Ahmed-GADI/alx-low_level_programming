8bd6f0d52929961cb73b6369311b126459bb496b
Ahmed GADI <ahmedgadi642@gmail.com>
2023-08-30 01:14:36 +0100

    More singly linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,6 +4,7 @@
  * get_nodeint_at_index - Entry point
  * @head: head of the list
  * @index: number of node
+ * Return: nth node
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
