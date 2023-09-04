5668f9728f9607051788554022c9996a9d37538f
Ahmed GADI <ahmedgadi642@gmail.com>
2023-09-04 22:05:14 +0100

    File i/o

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	
-	if (filename)
+	if (text_content)
 	{
 		for (leng = 0; text_content[leng];)
 			leng++;
