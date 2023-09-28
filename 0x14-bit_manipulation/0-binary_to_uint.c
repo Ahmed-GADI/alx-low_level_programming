15fbfa41ce4068b9b6aa5cb04191806931175a9b
Ahmed GADI <ahmedgadi642@gmail.com>
2023-09-29 00:09:11 +0100

    bit to uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Entry point
+ * @bin: binary number entered
+ * Return: the uint number
+ */
+
+unsigned int binary_to_uint(const char *bin)
+{
+	int i;
+	unsigned int decimal = 0;
+
+	if (!bin)
+		return (0);
+
+	for ( i = 0; bin[i]; i++)
+	{
+		if (bin[i] < '0' || bin[i] > '1')
+			return (0);
+
+		decimal = decimal * 2 + (bin[i] - '0');
+	}
+	return (decimal);
+}
