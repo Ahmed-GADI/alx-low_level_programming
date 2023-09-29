48d42fc11d7a01d66e5fd461f6436a90eec85113
Ahmed GADI <ahmedgadi642@gmail.com>
2023-09-29 14:28:06 +0100

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endiannes - Entry point
+ * Returns: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int i = 1;
+	char *ch = (char *) &i;
+	
+	return(*ch);
+}
